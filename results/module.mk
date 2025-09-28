028e0788353668cd4efb1a3c367c7830f1ab8229
BasharAbuLeil <bashar.ab@campus.technion.ac.il>
2025-09-28 22:08:03 +0300

    adding single threaded rule for the results

--- results/module.mk
+++ results/module.mk
@@ -11,8 +11,17 @@ result_measurements := $(foreach dir,$(result_measurements_dirs), \
 	$(foreach repeat,$(REPEATS), \
 		$(dir)/$(repeat)/time.csv))
 
+# Single-threaded variant: outputs under results/single_threaded/... and read from experiments-singlethreaded
+single_result_measurements_dirs := $(foreach malloc,$(MALLOC_VERSIONS), \
+	$(foreach bench,$(benchmarks), \
+		results/single_threaded/$(malloc)/$(bench)))
+
+single_result_measurements := $(foreach dir,$(single_result_measurements_dirs), \
+	$(foreach repeat,$(REPEATS), \
+		$(dir)/$(repeat)/time.csv))
+
 ##### rules
-.PHONY: results
+.PHONY: results results/single_threaded results/clean results/single_threaded_clean
 
 results: $(result_measurements)
 
@@ -23,5 +32,15 @@ results/%/time.csv:
 	mkdir -p $(dir $@)
 	$(kv_to_csv) $(patsubst results/%,experiments/%,$(basename $@).out) > $@
 
+# Single-threaded results target: generate results under results/single_threaded
+results/single_threaded: $(single_result_measurements)
+
+results/single_threaded/%/time.csv:
+	mkdir -p $(dir $@)
+	$(kv_to_csv) $(patsubst results/single_threaded/%,experiments-singlethreaded/%,$(basename $@).out) > $@
+
 results/clean:
-	rm -f $(result_measurements_dirs)
+	rm -f $(result_measurements)
+
+results/single_threaded_clean:
+	rm -f $(single_result_measurements)
